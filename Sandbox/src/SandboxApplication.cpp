f735b50203e60a1267f6223280ed554360b892fd
DESKTOP-2GGAN8E\DELL <3450548599@qq.com>
2024-10-23 12:56:04 +0800

    按键与鼠标代码，测试代码

--- Sandbox/src/SandboxApplication.cpp
+++ Sandbox/src/SandboxApplication.cpp
@@ -10,12 +10,21 @@ public:
 
 	void OnUpdate() override
 	{
-		HZ_INFO("ExampleLayer::Update");
+		
 	}
 
 	void OnEvent(Hazel::Event& event) override
 	{
-		HZ_TRACE("{0}", event);
+		if (event.GetEventType() == Hazel::EventType::KeyPressed)
+		{
+			Hazel::KeyPressedEvent& e = (Hazel::KeyPressedEvent&)event;
+			if (e.GetKeyCode() == HZ_KEY_TAB)
+			{
+				HZ_TRACE("Tab Key Pressed!");
+			}
+			HZ_TRACE("{0}", (char)e.GetKeyCode());
+			
+		}
 	}
 };
 
