cdea065053083b5e8f7fcb8595bf3a19ae4dbc71
shrimp <3450548599@qq.com>
2024-10-26 15:33:54 +0800

    渲染上下文

---
+++ Hazel/src/Platform/OpenGL/OpenGLContext.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Hazel/Renderer/GraphicsContext.h"
+
+struct GLFWwindow;
+
+namespace Hazel {
+
+	class OpenGLContext : public GraphicsContext
+	{
+	public:
+		OpenGLContext(GLFWwindow* windowHandle);
+		
+		void Init() override;
+		void SwapBuffers() override;
+
+	private:
+		GLFWwindow* m_WindowHandle;
+		
+	};
+
+}
--- Hazel/src/Platform/Windows/WindowsWindow.cpp
+++ Hazel/src/Platform/Windows/WindowsWindow.cpp
@@ -5,7 +5,7 @@
 #include "Hazel/Events/MouseEvent.h"
 #include "Hazel/Events/KeyEvent.h"
 
-#include <glad/glad.h>
+#include "Platform/OpenGL/OpenGLContext.h"
 
 namespace Hazel
 {
@@ -50,8 +50,10 @@ namespace Hazel
         }
 
         m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
-        glfwMakeContextCurrent(m_Window);
-        int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+        m_Context = new OpenGLContext(m_Window);
+        m_Context->Init();
+        
         glfwSetWindowUserPointer(m_Window, &m_Data);
         SetVSync(true);
         
@@ -155,7 +157,8 @@ namespace Hazel
     void WindowsWindow::OnUpdate()
     {
         glfwPollEvents();
-        glfwSwapBuffers(m_Window);
+        m_Context->SwapBuffers();
+        
     }
 
     void WindowsWindow::SetVSync(bool enabled)
