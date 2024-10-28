750f564cc753fbc2b9e7adf2f7170d96ea051457
DESKTOP-2GGAN8E\DELL <3450548599@qq.com>
2024-10-28 14:04:28 +0800

    编写Shader类

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -6,8 +6,8 @@
 #include "Hazel/LayerStack.h"
 #include "Hazel/Events/Event.h"
 #include "Hazel/Events/ApplicationEvent.h"
-
 #include "Hazel/ImGui/ImGuiLayer.h"
+#include "Hazel/Renderer/Shader.h"
 
 namespace Hazel {
 	class HAZEL_API Application
@@ -35,7 +35,8 @@ namespace Hazel {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
-
+		std::unique_ptr<Shader> m_Shader;
+		
 	private:
 		static Application* s_Instance;
 
