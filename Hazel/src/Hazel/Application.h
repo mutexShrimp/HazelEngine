824b244d1596e32b057d9be5f350c1ca6b2300d9
DESKTOP-2GGAN8E\DELL <3450548599@qq.com>
2024-10-30 16:50:26 +0800

    顶点数组

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -9,6 +9,7 @@
 #include "Hazel/ImGui/ImGuiLayer.h"
 #include "Hazel/Renderer/Shader.h"
 #include "Hazel/Renderer/Buffer.h"
+#include "Hazel/Renderer/VertexArray.h"
 
 namespace Hazel {
 	class HAZEL_API Application
@@ -35,14 +36,16 @@ namespace Hazel {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray;
-		std::unique_ptr<Shader> m_Shader;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		// unsigned int m_VertexArray;
+		std::shared_ptr<Shader> m_Shader;
+		std::shared_ptr<VertexArray> m_VertexArray;
+
+		std::shared_ptr<Shader> m_Shader2;
+		std::shared_ptr<VertexArray> m_SquareVA;
 		
 	private:
 		static Application* s_Instance;
-
+		
 	};
 
 	// To be defined in Client
