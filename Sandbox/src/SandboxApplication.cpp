9cd79d9f6dbe46b497be4a52e4d259d082a5d3b2
DESKTOP-2GGAN8E\DELL <3450548599@qq.com>
2024-11-01 19:33:47 +0800

    变换

--- Sandbox/src/SandboxApplication.cpp
+++ Sandbox/src/SandboxApplication.cpp
@@ -2,11 +2,15 @@
 
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public Hazel::Layer
 {
 public:
 	ExampleLayer()
-		: Layer("ExampleLayer"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f)
+		: Layer("ExampleLayer"),
+		m_Camera(-1.6f, 1.6f, -0.9f, 0.9f),
+		m_CameraPosition(0.0f)
 	{
 		m_VertexArray.reset(Hazel::VertexArray::Create());
 
@@ -61,6 +65,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 		
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -69,7 +74,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -96,13 +101,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 		
 			out vec3 v_Position;
 		
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -160,8 +166,19 @@ public:
 		m_Camera.SetRotation(m_CameraRotation);
 			
 		Hazel::Renderer::BeginScene(m_Camera);
-			
-		Hazel::Renderer::Submit(m_Shader2, m_SquareVA);
+		
+		static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for(int y = 0; y < 20; y++)
+		{
+			for(int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;	
+				Hazel::Renderer::Submit(m_Shader2, m_SquareVA, transform);
+			}
+		}
+		
 		Hazel::Renderer::Submit(m_Shader, m_VertexArray);
 			
 		Hazel::Renderer::EndScene();
