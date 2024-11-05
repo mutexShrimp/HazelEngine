17cd7a69069194271cdeb2b0d2e7e95419592161
DESKTOP-2GGAN8E\DELL <3450548599@qq.com>
2024-11-05 09:36:56 +0800

    混合

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -20,7 +20,8 @@ namespace Hazel {
 		m_Window = std::unique_ptr<Window>(Window::Create());
 		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 
-		//m_ImGuiLayer = std::make_unique<ImGuiLayer>();
+		Renderer::Init();
+		
 		m_ImGuiLayer = new ImGuiLayer();
 		PushOverlay(m_ImGuiLayer);
 		
