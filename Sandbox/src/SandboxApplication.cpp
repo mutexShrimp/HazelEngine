b324d4c11323c212b63b9d208d0f366a6b87c6f4
DESKTOP-2GGAN8E\DELL <3450548599@qq.com>
2024-11-07 16:57:43 +0800

    Preparing for 2D Rendering

--- Sandbox/src/SandboxApplication.cpp
+++ Sandbox/src/SandboxApplication.cpp
@@ -1,4 +1,6 @@
 #include <Hazel.h>
+// Entry Point
+#include <Hazel/Core/EntryPoint.h>
 
 #include "imgui/imgui.h"
 
@@ -7,6 +9,8 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include "SandboxApplication2D.h"
+
 class ExampleLayer : public Hazel::Layer
 {
 public:
@@ -14,7 +18,7 @@ public:
 		: Layer("ExampleLayer"),
 		m_CameraController(1280.0f / 720.0f, true)
 	{
-		m_VertexArray.reset(Hazel::VertexArray::Create());
+		m_VertexArray = Hazel::VertexArray::Create();
 
 		float vertices[3 * 7] = {
 			-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
@@ -37,7 +41,7 @@ public:
 		indexBuffer.reset(Hazel::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
 		m_VertexArray->SetIndexBuffer(indexBuffer);
 
-		m_SquareVA.reset(Hazel::VertexArray::Create());
+		m_SquareVA = Hazel::VertexArray::Create();
 
 		float squareVertices[5 * 4] = {
 			-0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
@@ -209,16 +213,17 @@ private:
 	
 };
 
-class Sandbox : public Hazel::Application
+class SandboxApplication : public Hazel::Application
 {
 public:
-	Sandbox()
+	SandboxApplication()
 	{
-		PushLayer(new ExampleLayer());
-
+		//PushLayer(new ExampleLayer());
+		PushLayer(new SandboxApplication2D());
+		
 	}
 
-	~Sandbox()
+	~SandboxApplication()
 	{
 
 	}
@@ -227,5 +232,5 @@ public:
 
 Hazel::Application* Hazel::CreateApplication()
 {
-	return new Sandbox();
+	return new SandboxApplication();
 }
---
+++ Sandbox/src/SandboxApplication2D.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "Hazel.h"
+
+class SandboxApplication2D : public Hazel::Layer
+{
+public:
+    SandboxApplication2D();
+    ~SandboxApplication2D() override = default;
+
+    void OnAttach() override;
+    void OnDetach() override;
+    void OnUpdate(Hazel::Timestep ts) override;
+    void OnImGuiRender() override;
+    void OnEvent(Hazel::Event& e) override;
+
+private:
+    Hazel::OrthographicCameraController m_CameraController;
+    
+    Hazel::Ref<Hazel::Shader> m_FlatColorShader;
+    Hazel::Ref<Hazel::VertexArray> m_SquareVA;
+    
+    glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
+};
