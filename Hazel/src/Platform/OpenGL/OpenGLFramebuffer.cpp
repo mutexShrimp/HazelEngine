13591c5e4838e63acd816463d05d958a3cfc814d
DESKTOP-2GGAN8E\DELL <3450548599@qq.com>
2024-11-13 18:34:23 +0800

    Framebuffers ----进行中

---
+++ Hazel/src/Platform/OpenGL/OpenGLFramebuffer.cpp
@@ -0,0 +1,2 @@
+﻿#include "hzpch.h"
+#include "OpenGLFramebuffer.h"
---
+++ Hazel/src/Platform/OpenGL/OpenGLFramebuffer.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+namespace Hazel
+{
+    class OpenGLFramebuffer : public Framebuffer
+    {
+    public:
+        
+    };
+}
