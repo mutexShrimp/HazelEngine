8a381eb36bdb0bafb4c33fe4259bf7bc949252e5
DESKTOP-2GGAN8E\DELL <3450548599@qq.com>
2024-11-13 10:10:36 +0800

    SubTextures

--- Hazel/src/Hazel/Renderer/SubTexture2D.h
+++ Hazel/src/Hazel/Renderer/SubTexture2D.h
@@ -14,7 +14,8 @@ namespace Hazel
         const Ref<Texture2D> GetTexture() const { return m_Texture; }
         const glm::vec2* GetTexCoords() const { return m_TexCoords; }
 
-        static Ref<SubTexture2D> CreateFromCoords(const Ref<Texture2D>& texture, const glm::vec2& coords, const glm::vec2& spriteSize);
+        static Ref<SubTexture2D> CreateFromCoords(const Ref<Texture2D>& texture, const glm::vec2& coords,
+            const glm::vec2& cellSize, const glm::vec2& spriteSize = {1, 1});
         
     private:
         Ref<Texture2D> m_Texture;
--- Sandbox/src/SandboxApplication2D.cpp
+++ Sandbox/src/SandboxApplication2D.cpp
@@ -15,6 +15,10 @@ void SandboxApplication2D::OnAttach()
 {
 	m_CheckerboardTexture = Hazel::Texture2D::Create("assets/textures/Checkerboard.png");
 	m_SpriteSheet = Hazel::Texture2D::Create("assets/game/textures/RPGpack_sheet_2X.png");
+
+	m_TextureStairs = Hazel::SubTexture2D::CreateFromCoords(m_SpriteSheet, {7, 6}, {128, 128});
+	m_TextureBarrel = Hazel::SubTexture2D::CreateFromCoords(m_SpriteSheet, {8, 2}, {128, 128});
+	m_TextureTree = Hazel::SubTexture2D::CreateFromCoords(m_SpriteSheet, {2, 1}, {128, 128}, {1, 2});
 	
 	m_Particle.ColorBegin = { 254 / 255.0f, 212 / 255.0f, 123 / 255.0f, 1.0f };
 	m_Particle.ColorEnd = { 254 / 255.0f, 109 / 255.0f, 41 / 255.0f, 1.0f };
@@ -79,6 +83,8 @@ void SandboxApplication2D::OnUpdate(Hazel::Timestep ts)
 
 	Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
 	Hazel::Renderer2D::DrawQuad({0.0f, 0.0f, 0.0f}, {1.0f, 1.0f}, m_TextureStairs);
+	Hazel::Renderer2D::DrawQuad({1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}, m_TextureBarrel);
+	Hazel::Renderer2D::DrawQuad({-1.0f, 0.0f, 0.0f}, {1.0f, 2.0f}, m_TextureTree);
 	Hazel::Renderer2D::EndScene();
 	
 	m_ParticleSystem.OnUpdate(ts);
