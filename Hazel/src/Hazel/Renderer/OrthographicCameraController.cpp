f7ad4e1513ffa6f041bb9862885273e3cb6e949d
DESKTOP-2GGAN8E\DELL <3450548599@qq.com>
2024-11-14 17:21:24 +0800

    Scene Viewport

--- Hazel/src/Hazel/Renderer/OrthographicCameraController.cpp
+++ Hazel/src/Hazel/Renderer/OrthographicCameraController.cpp
@@ -60,6 +60,12 @@ namespace Hazel
         dispatcher.Dispatch<WindowResizeEvent>(HZ_BIND_EVENT_FN(OrthographicCameraController::OnWindowResized));
     }
 
+    void OrthographicCameraController::OnResize(float width, float height)
+    {
+        m_AspectRatio = width / height;
+        CalculateView();
+    }
+
     void OrthographicCameraController::CalculateView()
     {
         m_Bounds = { -m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel };
@@ -77,8 +83,7 @@ namespace Hazel
 
     bool OrthographicCameraController::OnWindowResized(WindowResizeEvent& e)
     {
-        m_AspectRatio = (float)e.GetWidth() / (float)e.GetHeight();
-        CalculateView();
+        OnResize((float)e.GetWidth() , (float)e.GetHeight());
         return false;
     }
 }
