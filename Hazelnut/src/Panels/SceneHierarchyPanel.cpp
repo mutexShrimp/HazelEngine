d8739a28667f60eb617392fd46b2d0dcecf2bc9c
DESKTOP-2GGAN8E\DELL <3450548599@qq.com>
2024-11-21 19:02:58 +0800

    Properties Panel ----进行中

--- Hazelnut/src/Panels/SceneHierarchyPanel.cpp
+++ Hazelnut/src/Panels/SceneHierarchyPanel.cpp
@@ -80,9 +80,14 @@ namespace Hazel
 
         if (entity.HasComponent<TransformComponent>())
         {
-            auto& transform = entity.GetComponent<TransformComponent>().Transform;
+            if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+            {
+                auto& transform = entity.GetComponent<TransformComponent>().Transform;
 
-            ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.5f);
+                ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+                ImGui::TreePop();
+                
+            }
             
         }
         
