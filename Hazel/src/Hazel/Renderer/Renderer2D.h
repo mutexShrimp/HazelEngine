654437c2f5308b2253d5248a41f9e2e1d2e692d7
DESKTOP-2GGAN8E\DELL <3450548599@qq.com>
2024-11-29 18:02:45 +0800

    Editor Camera ----进行中

--- Hazel/src/Hazel/Renderer/Renderer2D.h
+++ Hazel/src/Hazel/Renderer/Renderer2D.h
@@ -14,7 +14,8 @@ namespace Hazel
         static void Shutdown();
 
         static void BeginScene(const Camera& camera, const glm::mat4& transform);
-        static void BeginScene(const OrthographicCamera& camera);
+    	static void BeginScene(const EditorCamera& camera);
+    	static void BeginScene(const OrthographicCamera& camera);
         static void EndScene();
         static void Flush();
         
--- Hazel/src/Hazel/Scene/Scene.h
+++ Hazel/src/Hazel/Scene/Scene.h
@@ -16,8 +16,9 @@ namespace Hazel
 
         Entity CreateEntity(const std::string& name = std::string());
         void DestroyEntity(Entity entity);
-        
-        void OnUpdate(Timestep ts);
+
+    	void OnUpdateEditor(Timestep ts, EditorCamera& camera);
+        void OnUpdateRuntime(Timestep ts);
         void OnViewportResize(uint32_t width, uint32_t height);
 
     	Entity GetPrimaryCameraEntity();
