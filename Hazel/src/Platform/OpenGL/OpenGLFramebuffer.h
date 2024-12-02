31c0972f41576a63b00eaafee2679836a89086d5
DESKTOP-2GGAN8E\DELL <3450548599@qq.com>
2024-12-02 16:38:50 +0800

    Multiple Render Targets and Framebuffer Refactor

--- Hazel/src/Platform/OpenGL/OpenGLFramebuffer.h
+++ Hazel/src/Platform/OpenGL/OpenGLFramebuffer.h
@@ -17,12 +17,11 @@ namespace Hazel
 
         void Resize(uint32_t width, uint32_t height) override;
         
-        uint32_t GetColorAttachmentRendererID() const override { return m_ColorAttachment; }
+        uint32_t GetColorAttachmentRendererID(uint32_t index = 0) const override { HZ_CORE_ASSERT(index < m_ColorAttachments.size()); return m_ColorAttachments[index]; }
         const FramebufferSpecification& GetSpecification() const override { return m_Specification; }
         
     private:
         uint32_t m_RendererID = 0;
-        uint32_t m_ColorAttachment = 0, m_DepthAttachment = 0;
         FramebufferSpecification m_Specification;
 
     	std::vector<FramebufferTextureSpecification> m_ColorAttachmentSpecifications;
